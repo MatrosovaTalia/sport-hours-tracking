6faeb79dc48e12e5220bb35bccdbcc965ba0caf7
MatrosovaTalia <zamoklaputa@gmail.com>
2020-03-14 23:31:19 +0300

    hide DB_URL

--- backend/sport_hours/config/common.py
+++ backend/sport_hours/config/common.py
@@ -1,7 +1,6 @@
 import os
 
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://natalia:holyshit@localhost:5433/sport_hours'
-# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.environ['DB_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024

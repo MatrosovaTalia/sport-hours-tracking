ac3e8bc3c4a9df1cd8b4834e574ad4bdd9e67158
illright <leva181777@gmail.com>
2020-03-15 15:54:15 +0300

    Set up the frontend skeleton

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Sport Hours Tracker</title>
+</svelte:head>
+
+<div style="display: flex; flex-direction: column;">
+	<a href="/track-attendance" rel="prefetch">Track attendance</a>
+	<a href="/check-hours" rel="prefetch">View attendance statistics</a>
+</div>

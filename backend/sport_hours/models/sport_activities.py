5c01a504e81838b8df6a8b0d07cee8ce64e837b4
illright <leva181777@gmail.com>
2020-03-15 13:42:48 +0300

    Fix up model naming, add a unique constraint

--- backend/sport_hours/models/sport_activities.py
+++ backend/sport_hours/models/sport_activities.py
@@ -1,13 +1,14 @@
 from sport_hours.extensions import db
 
-class SportActivities(db.Model):
-    __tablename__ = 'sport_activities'
 
+class SportActivity(db.Model):
+    __tablename__ = 'sport_activities'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
 
-class Clubs(db.Model):
+
+class Club(db.Model):
     __tablename__ = 'clubs'
 
     id = db.Column(db.Integer, db.ForeignKey('sport_activities.id'), primary_key=True)

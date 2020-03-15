396131aad594e7f618943f919ef60380db24fefe
illright <leva181777@gmail.com>
2020-03-15 14:45:24 +0300

    Make activity_assignment a regular table

--- backend/sport_hours/models/user.py
+++ backend/sport_hours/models/user.py
@@ -1,10 +1,12 @@
 from sport_hours.extensions import db
 from sqlalchemy.dialects import postgresql
 
+
 class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(64), nullable=False)    
+    email = db.Column(db.String(64), nullable=False)
     roles = db.Column(postgresql.ARRAY(db.String(32)), nullable=False)
+    # property `activities` available with a backref

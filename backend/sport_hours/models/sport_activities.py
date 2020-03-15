dded541d733b7453cf48dc8c289eb68edff71a9c
illright <leva181777@gmail.com>
2020-03-15 14:45:24 +0300

    Make activity_assignment a regular table

--- backend/sport_hours/models/sport_activities.py
+++ backend/sport_hours/models/sport_activities.py
@@ -1,11 +1,26 @@
 from sport_hours.extensions import db
 
 
+activity_assignment = db.Table(
+    'activity_assignment',
+    db.Column('activity_id', db.Integer,
+              db.ForeignKey('sport_activities.id', ondelete='CASCADE'),
+              primary_key=True),
+    db.Column('student_id', db.Integer,
+              db.ForeignKey('users.id', ondelete='CASCADE'),
+              primary_key=True)
+)
+
+
 class SportActivity(db.Model):
     __tablename__ = 'sport_activities'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
+    assigned_students = db.relationship('User',
+                                        secondary=activity_assignment,
+                                        lazy=True,
+                                        backref=db.backref('activities', lazy=True))
 
 
 class Club(db.Model):

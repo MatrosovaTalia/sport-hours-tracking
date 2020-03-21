ded4695675cd5eeb312f544282262e8fda750d00
Vladislav Kantaev <vvkantaev@yandex.ru>
2020-03-22 01:41:39 +0300

    Add CRUD for sport activities

--- backend/sport_hours/views/sport_activities.py
+++ backend/sport_hours/views/sport_activities.py
@@ -1,8 +1,10 @@
-from flask import request
+from flask import request, abort
+from flask.views import MethodView
 
+from sport_hours.extensions import db
 from sport_hours.blueprints import api
-from sport_hours.models import User, SportActivity
-from sport_hours.schemas import SportActivitySchema
+from sport_hours.models import User, SportActivity, Club
+from sport_hours.schemas import SportActivitySchema, ClubSchema
 
 
 @api.route('/activities')
@@ -18,8 +20,85 @@ def get_all_activities():
     out_schema = SportActivitySchema(many=True, exclude=('assigned_students',))
     return out_schema.jsonify(activities)
 
+
+@api.route('/activities/clubs/all')
+def get_all_clubs():
+    clubs = Club.query.all()
+    out_schema = ClubSchema(many=True)
+    return out_schema.jsonify(clubs)
+
+
 @api.route('/activity')
 def get_activity_by_id():
-    activity = SportActivity.query.first_or_404(request.args.get('id'))
+    activity = SportActivity.query.filter_by(id=request.args.get('id')).one_or_none()
+
+    if activity is None:
+        return ('', 404)
+
     out_schema = SportActivitySchema(many=False, exclude=('assigned_students',))
     return out_schema.jsonify(activity)
+
+
+@api.route('/activities/create', methods=['POST'])
+def create_sport_activity():
+    in_schema = SportActivitySchema(exclude=('id',))
+    activity_record = in_schema.load(request.json)
+
+    db.session.add(activity_record)
+    db.session.commit()
+
+    return ('', 204)
+
+
+@api.route('/activities/<int:id>', methods=['PUT'])
+def modify_sport_activity(id):
+    in_schema = SportActivitySchema(exclude=('id',))
+    record_in_db = SportActivity.query.filter_by(id=id).one_or_none()
+
+    if record_in_db is None:
+        return ('', 404)
+
+    in_schema.load(request.json, session=db.session, instance=record_in_db, partial=True)
+    
+    db.session.commit()
+    
+    return ('', 204)
+
+
+@api.route('/activities/<int:id>', methods=['DELETE'])
+def delete_activity(id):
+    SportActivity.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    return ('', 204)
+
+
+@api.route('/activities/clubs/<int:id>', methods=['PUT'])
+def create_club(id):
+    activity = SportActivity.query.filter_by(id=id).one_or_none()
+    
+    if activity is None:
+        return ('', 404)
+
+    in_schema = ClubSchema(exclude=('id',))
+    record_in_db = Club.query.filter_by(id=id).one_or_none()
+
+    if record_in_db:
+        in_schema.load(request.json, session=db.session, instance=record_in_db, partial=True)
+    else:
+        club_record = in_schema.load(request.json)
+        club_record.id = id
+
+        db.session.add(club_record)
+    
+    db.session.commit()
+
+    return ('', 204)
+
+
+@api.route('/activities/clubs/<int:id>', methods=['DELETE'])
+def delete_club(id):
+    Club.query.filter_by(id=id).delete()
+    db.session.commit()
+
+    return ('', 204)

f5da7210967368602f5373f4e338d5fe9d20ea39
mari1647iv <51486276+mari1647iv@users.noreply.github.com>
2020-04-07 15:08:11 +0300

    Update index with activity assignment features

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -5,6 +5,12 @@
 <div style="display: flex; flex-direction: column;">
 	<a href="/track-attendance" rel="prefetch">Track attendance</a>
 	<a href="/check-hours" rel="prefetch">View attendance statistics</a>
-	
+<<<<<<< HEAD
 	<a href="/login" rel="prefetch">Choose user</a>
+	
+=======
+	<a href="/student-activity-assignment" rel="prefetch">Choose sport activity</a>
+	<a href="/admin-activity-assignment" rel="prefetch">Assign student to sport activity</a>
+	<a href="/login" rel="prefetch">Choose user</a>
+>>>>>>> Update index with activity assignment features
 </div>

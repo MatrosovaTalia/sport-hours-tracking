26e70e491554ef2c63bbf74eb8d53d481fa8c00e
mari1647iv <51486276+mari1647iv@users.noreply.github.com>
2020-04-07 15:20:47 +0300

    Small code style fixes

--- frontend/src/routes/student-activity-assignment.svelte
+++ frontend/src/routes/student-activity-assignment.svelte
@@ -1,48 +1,48 @@
 <script>
     let activities = [
         {id: '10', name: 'Breakdance, Friday, 17:00-18:30'},
-			  {id: '11', name: 'Breakdance, Saturday, 13:00-14:30'},
+	{id: '11', name: 'Breakdance, Saturday, 13:00-14:30'},
         {id: '20', name: 'Swimming, Friday, 9:00-10:00'},
-			  {id: '21', name: 'Swimming, Wednessday, 9:00-10:00'},
+	{id: '21', name: 'Swimming, Wednessday, 9:00-10:00'},
         {id: '3', name: 'Stretching, Monday, 15:00-16:30'},
         {id: '4', name: 'Fitness trainings, Thursday, 8:30-10:00'},
         {id: '5', name: 'Backetball, Wednessday, 18:00-20:00'},
     ];
-		let chosenUser = 1;
-		let chosenActivity;
-		let assignments = [{ActivityID: 10, StudentID: chosenUser}];
+	let chosenUser = 1;
+	let chosenActivity;
+	let assignments = [{ActivityID: 10, StudentID: chosenUser}];
 
-		function assignStudent() {
-			assignments.push({ActivityID: chosenActivity, StudentID: chosenUser});
-			assignments = assignments;
+	function assignStudent() {
+		assignments.push({ActivityID: chosenActivity, StudentID: chosenUser});
+		assignments = assignments;
 	}
 </script>
 
 <div>
         <p id = "textp">Choose sport activity to assign yourself:<p>
-				<p><select name="Activity" bind:value={chosenActivity}>
-            <option disabled selected>--Choose  Sport Activity--</option>
-            {#each activities as activity(activity.id)}
-            <option value={activity.id}>{activity.name}</option>
-            {/each}
+	<p><select name="Activity" bind:value={chosenActivity}>
+            	<option disabled selected>--Choose  Sport Activity--</option>
+            	{#each activities as activity(activity.id)}
+            		<option value={activity.id}>{activity.name}</option>
+            	{/each}
         </select></p>
-        <div>
-            <input type="submit" value="Assign" on:click={assignStudent}>
+	<div>
+            	<input type="submit" value="Assign" on:click={assignStudent}>
         </div>
 	{#if assignments != null}
-    <table>
-      <thead>
-        <th>Your Sport Activities</th>
-      </thead>
-      <tbody>
+    	<table>
+      	<thead>
+        	<th>Your Sport Activities</th>
+      	</thead>
+      	<tbody>
         {#each assignments as assignment (assignment.ActivityID)}
-          <tr>
-            <td>{assignment.ActivityID}</td>
-          </tr>
+          	<tr>
+            		<td>{assignment.ActivityID}</td>
+          	</tr>
         {/each}
-      </tbody>
-    </table>
-  {/if}
+      	</tbody>
+    	</table>
+  	{/if}
 </div>
 
 <style>

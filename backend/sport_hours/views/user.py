0e017dae3ac1ae19e68e2923c491d93f85249dcc
illright <leva181777@gmail.com>
2020-04-10 11:42:28 +0300

    Clean up the codebase

--- backend/sport_hours/views/user.py
+++ backend/sport_hours/views/user.py
@@ -3,11 +3,6 @@ from sport_hours.models import User
 from sport_hours.schemas import UserSchema
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(user_id)
-
-
 @api.route('/users')
 def get_users():
     out_schema = UserSchema(many=True, exclude=('activities', 'roles'))

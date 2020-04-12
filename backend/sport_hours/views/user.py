9a83147e0a96b2fbd3a5b9e3bac633e4c5509ff1
illright <leva181777@gmail.com>
2020-04-12 23:37:58 +0300

    Add the own account endpoint

--- backend/sport_hours/views/user.py
+++ backend/sport_hours/views/user.py
@@ -1,3 +1,5 @@
+from flask_login import login_required, current_user
+
 from sport_hours.blueprints import api
 from sport_hours.models import User
 from sport_hours.schemas import UserSchema
@@ -7,3 +9,10 @@ from sport_hours.schemas import UserSchema
 def get_users():
     out_schema = UserSchema(many=True, exclude=('activities', 'roles'))
     return out_schema.jsonify(User.query.all())
+
+
+@api.route('/user')
+@login_required
+def get_self():
+    out_schema = UserSchema(exclude=('activities',))
+    return out_schema.jsonify(current_user)

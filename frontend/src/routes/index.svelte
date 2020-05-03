2d590d711ce37ebb8be456ac0b9987197c15922c
illright <leva181777@gmail.com>
2020-05-03 12:48:41 +0300

    Add user information to the home page

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,12 +1,43 @@
+<script context="module">
+  import getInitialData from '@/utils/get-initial-data.js';
+  export async function preload(page, session) {
+    return await getInitialData(this, session, new Map([
+      ['currentUser', '/user'],
+    ]));
+  }
+</script>
+
+<script>
+	export let currentUser;
+</script>
+
 <svelte:head>
 	<title>Sport Hours Tracker</title>
 </svelte:head>
 
+<strong>
+	{#if currentUser == null}
+		Not logged in
+	{:else}
+		Logged in as {currentUser.full_name}{currentUser.is_admin ? ' (admin)': ''}
+	{/if}
+</strong>
 <div style="display: flex; flex-direction: column;">
-	<a href="/activities">Browse activities</a>
-	<a href="/check-hours" rel="prefetch">View attendance statistics</a>
-	<a href="/student-activity-assignment" rel="prefetch">Choose sport activity</a>
-	<a href="/admin-activity-assignment" rel="prefetch">Assign student to sport activity</a>
+	{#if currentUser != null}
+		<a href="/activities">Browse activities</a>
+		<a href="/check-hours" rel="prefetch">View attendance statistics</a>
+		<a href="/student-activity-assignment" rel="prefetch">Choose sport activity</a>
+		{#if currentUser.is_admin}
+			<a href="/admin-activity-assignment" rel="prefetch">Assign unassigned students</a>
+		{/if}
+	{/if}
 
 	<a href="/log-in" rel="prefetch">Choose user</a>
 </div>
+
+<style>
+	strong {
+		display: block;
+		margin-bottom: 1em;
+	}
+</style>

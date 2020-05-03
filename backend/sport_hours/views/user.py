463cc50dde1d15d07f8c85cf642ba81385c2afb9
illright <leva181777@gmail.com>
2020-05-03 12:15:18 +0300

    Implement retrieving all unassigned students

--- backend/sport_hours/views/user.py
+++ backend/sport_hours/views/user.py
@@ -1,15 +1,14 @@
-
-from flask import abort, request
+from flask import abort
 from flask_login import login_required, current_user
 
 from sport_hours.blueprints import api
-from sport_hours.models import User
+from sport_hours.extensions import db
+from sport_hours.models import User, activity_assignment
 from sport_hours.schemas import UserSchema
 
 
 @api.route('/users')
 def get_users():
-
     out_schema = UserSchema(many=True, exclude=('activities',))
     return out_schema.jsonify(User.query.all())
 
@@ -19,3 +18,15 @@ def get_users():
 def get_self():
     out_schema = UserSchema(exclude=('activities',))
     return out_schema.jsonify(current_user)
+
+
+@api.route('/users/unassigned')
+@login_required
+def get_unassigned():
+    if not current_user.is_admin:
+        abort(403)
+
+    activity_count = db.func.count(activity_assignment.c.activity_id)
+    users = User.query.outerjoin(activity_assignment).group_by(User).having(activity_count == 0)
+    out_schema = UserSchema(exclude=('activities',), many=True)
+    return out_schema.jsonify(users.all())

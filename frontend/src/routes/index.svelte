a179b667c95bad8223a155aafe0c478ab7dbbdeb
illright <leva181777@gmail.com>
2020-05-07 18:48:25 +0300

    Implement the activity creation form

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -29,6 +29,7 @@
 		<a href="/student-activity-assignment" rel="prefetch">Choose sport activity</a>
 		{#if currentUser.is_admin}
 			<a href="/admin-activity-assignment" rel="prefetch">Assign unassigned students</a>
+			<a href="/create-activity" rel="prefetch">Create a new sport activity</a>
 		{/if}
 	{/if}
 

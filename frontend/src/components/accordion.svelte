dec04e8a2f6463f3b719a81f7059add479f7d187
illright <leva181777@gmail.com>
2020-05-07 18:47:54 +0300

    Steal a bunch of components

---
+++ frontend/src/components/accordion.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let classname = '';
+
+  let closeCallback = null;
+  function panelController(event) {
+    if (closeCallback !== null && closeCallback !== event.detail.closeCallback) {
+      closeCallback();
+    }
+    closeCallback = event.detail.closeCallback;
+  }
+</script>
+
+<style>
+  ul {
+    list-style: none;
+  }
+</style>
+
+<ul class="accordion {classname}">
+  <slot {panelController}></slot>
+</ul>
